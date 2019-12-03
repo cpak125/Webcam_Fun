959b01370f41e610f7a591c5f07081694335d7a3
Chris Pak <chris.pak125@gmail.com>
2019-12-03 17:01:47 -0500

    Video painted to canvas

---
+++ script.js
@@ -0,0 +1,31 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap')
+
+getVideo = () => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            console.log(localMediaStream)
+            video.srcObject = localMediaStream
+            video.play()
+        })
+        .catch(err => {
+            console.console.error(`OH NOO!!!`, err);
+
+        })
+}
+
+paintToCanvas = () => {
+    const width = video.videoWidth
+    const height = video.videoHeight
+    canvas.width = width
+    canvas.height = height
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height)
+    }, 16)
+}
+
+getVideo()
